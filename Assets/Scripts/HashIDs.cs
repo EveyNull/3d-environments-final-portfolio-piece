eaa6944c833d6a196da8c3afe2ee68022c4bef75
Evey Watson <taunull47@gmail.com>
2019-02-28 11:07:33 +0000

    Initial level

---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashIDs : MonoBehaviour {
+
+    public int speedFloat;
+    public int walkState;
+
+    // Use this for initialization
+    void Start () {
+        speedFloat = Animator.StringToHash("Speed");
+        walkState = Animator.StringToHash("Walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speedDampTime = 0.01f;
+    public float sensitivityX = 1.0f;
+
+    private Animator anim;
+    private HashIDs hash;
+    
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float v = Input.GetAxis("Vertical");
+        //float mouseX = Input.GetAxis("MouseX");
+
+        //Rotating(mouseX);
+        MovementManager(v);
+	}
+
+    void MovementManager(float vertical)
+    {
+        if(vertical>0)
+        {
+            anim.SetFloat(hash.speedFloat, 1.5f, speedDampTime, Time.deltaTime);
+        }
+        else
+        {
+            anim.SetFloat(hash.speedFloat, 0);
+        }
+    }
+
+    void Rotating(float mouseXInput)
+    {
+
+    }
+}
