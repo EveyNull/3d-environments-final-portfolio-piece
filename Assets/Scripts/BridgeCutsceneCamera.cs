448a0ac9ed7c7d250043d963f5e2932ffb362e77
Evey Watson <taunull47@gmail.com>
2019-03-07 23:34:35 +0000

    Lever triggers animation

---
+++ Assets/Scripts/BridgeCutsceneCamera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeCutsceneCamera : MonoBehaviour {
+
+    public Transform bridgeCutsceneDest;
+    public Transform overShoulderDest;
+    public GameObject playerCameraParent;
+    public float speed = 3f;
+    public float pauseTime = 1f;
+
+    private bool cutscenePlaying = false;
+    private bool movingToCutsceneDest = true;
+    private float timeElapsed = 0f;
+    private float pauseTimer = 0f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cutscenePlaying)
+        {
+            Debug.Log(overShoulderDest.position);
+            timeElapsed += Time.deltaTime;
+            float step = speed * Time.deltaTime;
+            if (movingToCutsceneDest)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, bridgeCutsceneDest.position, step);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, bridgeCutsceneDest.rotation, step * 3.5f);
+                if (transform.position == bridgeCutsceneDest.position
+                    && transform.rotation == bridgeCutsceneDest.rotation)
+                {
+                    movingToCutsceneDest = false;
+                }
+            }
+            else
+            {
+                pauseTimer += Time.deltaTime;
+                if (pauseTimer > pauseTime)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, overShoulderDest.position, step);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, overShoulderDest.rotation, step*3.5f);
+                    if (transform.position == overShoulderDest.position
+                        && transform.rotation == overShoulderDest.rotation)
+                    {
+                        cutscenePlaying = false;
+                        transform.parent = playerCameraParent.transform;
+                        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = true;
+                        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().enabled = true;
+                    }
+                }
+            }
+        }
+	}
+
+    public void StartCutscene()
+    {
+        cutscenePlaying = true;
+        transform.parent = null;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = false;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour {
+
+    public GameObject leverBase;
+    public GameObject bridge;
+
+    private bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("InteractionCollider")
+            && Input.GetAxis("Interact") == 1
+            && !triggered)
+        {
+            triggered = true;
+            MoveToPosition[] bridgeComponents = bridge.GetComponentsInChildren<MoveToPosition>();
+            foreach (MoveToPosition component in bridgeComponents)
+            {
+                component.enabled = true;
+            }
+            leverBase.GetComponent<MoveToPosition>().enabled = true;
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<BridgeCutsceneCamera>().StartCutscene();
+        }
+    }
+}
